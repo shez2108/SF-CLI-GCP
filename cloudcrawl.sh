cd51cb0cc5c2ad30e82a45aaacc5428816da9f2e
shez2108 <75090807+shez2108@users.noreply.github.com>
2023-08-03 13:06:47 +0100

    Update cloudcrawl.sh

--- cloudcrawl.sh
+++ cloudcrawl.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/bash
 
-# ask the user what domain they want to crawl 
+# ask the user what domain they want to crawl, entered domain is stored in the variable #domain 
 echo "Enter domain to crawl"
 
 read domain
 
 # initiate crawl from screamingfrogseospider without GUI (headless) and define the output folder as the crawl-data directory 
 screamingfrogseospider --crawl $domain --headless --output-folder ~/crawl-data/ \
---export-tabs "Internal:All,Directives:All" --overwrite --bulk-export "All Inlinks" # data should be exported from these tabs in the .deb
+--export-tabs "Internal:All,Directives:All" --overwrite --bulk-export "All Inlinks" # data should be exported from these tabs in the .deb 
 
-# defines filename based on date and domain 
-now=$(date +"%Y_%m_%d")
-filename=${domain//./_}
-filename="$filename"_"$now"
+
+now=$(date +"%Y_%m_%d") #defines date 
+filename=${domain//./_} # defines filename based on domain name
+#filename="$filename"_"$now"
 bq mk ${filename} # creates directory in bigquery based on the filename 
 
 # replace null values with spaces in the csv files 
-tr '\0' ' ' < ~/crawl-data/${filename}_internal_all_${now}.csv > ~/crawl-data/${filename}_internal_all_clean_${now}.csv
-tr '\0' ' ' < ~/crawl-data/${filename}_directives_all_${now}.csv > ~/crawl-data/${filename}_directives_all_clean_${now}.csv
-tr '\0' ' ' < ~/crawl-data/${filename}_all_inlinks_${now}.csv > ~/crawl-data/${filename}_all_inlinks_clean_${now}.csv
+tr '\0' ' ' < ~/crawl-data/internal_all_${now}.csv > ~/crawl-data/internal_all_clean${now}.csv
+tr '\0' ' ' < ~/crawl-data/directives_all${now}.csv > ~/crawl-data/directives_all_clean${now}.csv
+tr '\0' ' ' < ~/crawl-data/all_inlinks${now}.csv > ~/crawl-data/all_inlinks_clean${now}.csv
 
 bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
 ${filename}.screaming_frog_crawls_internal_all_${now} ~/crawl-data/${filename}_internal_all_${now}.csv

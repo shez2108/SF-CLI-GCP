cfd26fe24d3f673533b1610add7dfa96354dcd21
shez2108 <75090807+shez2108@users.noreply.github.com>
2023-08-03 11:10:12 +0100

    Update cloudcrawl.sh

--- cloudcrawl.sh
+++ cloudcrawl.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/bash
 
+# ask the user what domain they want to crawl 
 echo "Enter domain to crawl"
 
 read domain
 
+# initiate crawl from screamingfrogseospider without GUI (headless) and define the output folder as the crawl-data directory 
 screamingfrogseospider --crawl $domain --headless --output-folder ~/crawl-data/ \
---export-tabs "Internal:All,Directives:All" --overwrite --bulk-export "All Inlinks"
+--export-tabs "Internal:All,Directives:All" --overwrite --bulk-export "All Inlinks" # data should be exported from these tabs in the .deb
 
+# defines filename based on date and domain 
 now=$(date +"%Y_%m_%d")
 filename=${domain//./_}
 filename="$filename"_"$now"
-bq mk ${filename}
+bq mk ${filename} # creates directory in bigquery based on the filename 
 
+# replace null values with spaces in the csv files 
 tr '\0' ' ' < ~/crawl-data/${filename}_internal_all_${now}.csv > ~/crawl-data/${filename}_internal_all_clean_${now}.csv
 tr '\0' ' ' < ~/crawl-data/${filename}_directives_all_${now}.csv > ~/crawl-data/${filename}_directives_all_clean_${now}.csv
 tr '\0' ' ' < ~/crawl-data/${filename}_all_inlinks_${now}.csv > ~/crawl-data/${filename}_all_inlinks_clean_${now}.csv

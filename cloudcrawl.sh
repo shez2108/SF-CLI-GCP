c0783641d3f3d8f3e3a389127a0701c95b81b543
shez2108 <75090807+shez2108@users.noreply.github.com>
2023-08-04 15:01:25 +0100

    Update cloudcrawl.sh

--- cloudcrawl.sh
+++ cloudcrawl.sh
@@ -17,17 +17,17 @@ filename=${domain//./_} # defines filename based on domain name
 bq mk ${filename}_sf_crawls # creates dataset in bigquery based on the filename 
 
 # replace null values with spaces in the csv files 
-tr '\0' ' ' < ~/crawl-data/internal_all_${now}.csv > ~/crawl-data/internal_all_clean${now}.csv
-tr '\0' ' ' < ~/crawl-data/directives_all${now}.csv > ~/crawl-data/directives_all_clean${now}.csv
-tr '\0' ' ' < ~/crawl-data/all_inlinks${now}.csv > ~/crawl-data/all_inlinks_clean${now}.csv
+tr '\0' ' ' < ~/crawl-data/internal_all.csv > ~/crawl-data/internal_all_clean${now}.csv
+tr '\0' ' ' < ~/crawl-data/directives_all.csv > ~/crawl-data/directives_all_clean${now}.csv
+tr '\0' ' ' < ~/crawl-data/all_inlinks.csv > ~/crawl-data/all_inlinks_clean${now}.csv
 
 bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
-${filename}_sf_crawls._internal${now} ~/crawl-data/internal_all_clean${now}.csv
+${filename}_sf_crawls._internal ~/crawl-data/internal_all_clean${now}.csv
 
 bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
-${filename}_sf_crawls._directives${now} ~/crawl-data/directives_all_clean${now}.csv
+${filename}_sf_crawls._directives ~/crawl-data/directives_all_clean${now}.csv
 
 bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
-${filename}_sf_crawls._inlinks${now} ~/crawl-data/all_inlinks_clean${now}.csv
+${filename}_sf_crawls._inlinks ~/crawl-data/all_inlinks_clean${now}.csv
 
 curl -i -H "Content-Type:application/json; charset=UTF-8" --data '{"text":"'"$domain"' crawl complete"}' "https://chat.googleapis.com/{token}"

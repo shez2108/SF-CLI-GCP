cd9ca4db2d28ec21d0b7c7b304d692763c168549
shez2108 <75090807+shez2108@users.noreply.github.com>
2023-08-04 15:03:40 +0100

    Update cloudcrawl.sh

--- cloudcrawl.sh
+++ cloudcrawl.sh
@@ -22,12 +22,12 @@ tr '\0' ' ' < ~/crawl-data/directives_all.csv > ~/crawl-data/directives_all_clea
 tr '\0' ' ' < ~/crawl-data/all_inlinks.csv > ~/crawl-data/all_inlinks_clean${now}.csv
 
 bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
-${filename}_sf_crawls._internal ~/crawl-data/internal_all_clean${now}.csv
+${filename}_sf_crawls._internal${now} ~/crawl-data/internal_all_clean.csv
 
 bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
-${filename}_sf_crawls._directives ~/crawl-data/directives_all_clean${now}.csv
+${filename}_sf_crawls._directives${now} ~/crawl-data/directives_all_clean.csv
 
 bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
-${filename}_sf_crawls._inlinks ~/crawl-data/all_inlinks_clean${now}.csv
+${filename}_sf_crawls._inlinks${now} ~/crawl-data/all_inlinks_clean.csv
 
 curl -i -H "Content-Type:application/json; charset=UTF-8" --data '{"text":"'"$domain"' crawl complete"}' "https://chat.googleapis.com/{token}"

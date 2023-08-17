321625e01143f9df92c70f3507388735bfa106a7
shez2108 <75090807+shez2108@users.noreply.github.com>
2023-08-17 09:30:35 +0100

    Update cloudcrawl.sh

--- cloudcrawl.sh
+++ cloudcrawl.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/bash
 
-# Ask the user what domain they want to crawl. The entered domain is stored in the variable #domain 
+# ask the user what domain they want to crawl, entered domain is stored in the variable #domain
 echo "Enter domain to crawl"
+
 read domain
 
-# Path to the .seospiderconfig file
-CONFIG_PATH="/home/clients/SEO\ Spider\ Config.seospiderconfig"
+# initiate crawl from screamingfrogseospider without GUI (headless) and define the output folder as the crawl-data directory
+screamingfrogseospider --crawl $domain --headless --output-folder ~/crawl-data/ \
+--export-tabs "Internal:All,Directives:All" --overwrite --bulk-export "All Inlinks" # data should be exported from these tabs in the .deb
 
-# Initiate crawl from screamingfrogseospider without GUI (headless) and define the output folder as the crawl-data directory 
-screamingfrogseospider --crawl $domain --headless --config $CONFIG_PATH --output-folder ~/crawl-data/ \
---export-tabs "Internal:All,Directives:All" --overwrite --bulk-export "All Inlinks" 
 
-now=$(date +"%Y_%m_%d") # Defines date 
-filename=${domain//./_} # Defines filename based on domain name
+now=$(date +"%Y_%m_%d") #defines date
+filename=${domain//./_} # defines filename based on domain name
+filename=${filename//-/_} # replace '-' with '_'
+filename=${filename////_} # replace '/' with '_'
+#filename="$filename"_"$now"
 
-bq mk ${filename}_sf_crawls # Creates dataset in bigquery based on the filename 
+bq mk ${filename}_sf_crawls # creates dataset in bigquery based on the filename
 
-# Replace null values with spaces in the csv files 
+# replace null values with spaces in the csv files
 tr '\0' ' ' < ~/crawl-data/internal_all.csv > ~/crawl-data/internal_all_clean.csv
 tr '\0' ' ' < ~/crawl-data/directives_all.csv > ~/crawl-data/directives_all_clean.csv
 tr '\0' ' ' < ~/crawl-data/all_inlinks.csv > ~/crawl-data/all_inlinks_clean.csv
@@ -31,4 +33,3 @@ bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_
 ${filename}_sf_crawls.inlinks${now} ~/crawl-data/all_inlinks_clean.csv
 
 curl -i -H "Content-Type:application/json; charset=UTF-8" --data '{"text":"'"$domain"' crawl complete"}' "https://chat.googleapis.com/{token}"
-

82028e86213b1b46d061305857bc981a449dc703
shez2108 <75090807+shez2108@users.noreply.github.com>
2023-08-17 10:57:22 +0100

    Update cloudcrawl.sh

--- cloudcrawl.sh
+++ cloudcrawl.sh
@@ -22,6 +22,7 @@ bq mk ${filename}_sf_crawls # creates dataset in bigquery based on the filename
 tr '\0' ' ' < ~/crawl-data/internal_all.csv > ~/crawl-data/internal_all_clean.csv
 tr '\0' ' ' < ~/crawl-data/directives_all.csv > ~/crawl-data/directives_all_clean.csv
 tr '\0' ' ' < ~/crawl-data/all_inlinks.csv > ~/crawl-data/all_inlinks_clean.csv
+tr '\0' ' ' < ~/crawl-data/all_inlinks.csv > ~/crawl-data/hreflang_all_clean.csv
 
 bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
 ${filename}_sf_crawls.internal${now} ~/crawl-data/internal_all_clean.csv
@@ -30,6 +31,9 @@ bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_
 ${filename}_sf_crawls.directives${now} ~/crawl-data/directives_all_clean.csv
 
 bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
-${filename}_sf_crawls.inlinks${now} ~/crawl-data/all_inlinks_clean.csv
+${filename}_sf_crawls.inlinks_${now} ~/crawl-data/all_inlinks_clean.csv
+
+bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
+${filename}_sf_crawls.hreflang.csv${now} ~/crawl-data/hreflang_all_clean.csv
 
 curl -i -H "Content-Type:application/json; charset=UTF-8" --data '{"text":"'"$domain"' crawl complete"}' "https://chat.googleapis.com/{token}"

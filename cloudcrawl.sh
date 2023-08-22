9a28076a534d0e1579c7955864ca70a5418efa60
shez2108 <75090807+shez2108@users.noreply.github.com>
2023-08-22 10:26:50 +0100

    Update cloudcrawl.sh

--- cloudcrawl.sh
+++ cloudcrawl.sh
@@ -9,7 +9,7 @@ configpath="/home/clients/Config.seospiderconfig"
 
 # initiate crawl from screamingfrogseospider without GUI (headless) and define the output folder as the crawl-data directory
 screamingfrogseospider --crawl "$domain" --headless --config "$configpath" --output-folder ~/crawl-data/ \
---export-tabs "Internal:All,Directives:All,Hreflang:All,Pagination:All,Structured Data:All,Sitemaps:All" --overwrite --bulk-export "All Inlinks,All Content" 
+--export-tabs "Internal:All,Directives:All,Hreflang:All,Pagination:All,Structured Data:All,Sitemaps:All" --overwrite --bulk-export "All Inlinks" 
 # data should be exported from these tabs in the .deb
 
 #JavaScript:All,Links:All
@@ -26,73 +26,70 @@ if ! bq ls ${filename}_sf_crawls &>/dev/null; then
 fi
 
 # Check if the file exists
-#if [ ! -f ~/crawl-data/internal_all.csv ]; then
-    #echo "Error: internal_all.csv does not exist."
-    #exit 1
-#fi
-# replace null values with spaces in the csv files
-#tr '\0' ' ' < ~/crawl-data/internal_all.csv > ~/crawl-data/internal_all_clean.csv
-
-#bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
-#${filename}_sf_crawls.internal${now} ~/crawl-data/internal_all_clean.csv
-
-#if [ ! -f ~/crawl-data/directives_all.csv ]; then
-    #echo "Error: directives_all.csv does not exist."
-    #exit 1
-#fi
-#tr '\0' ' ' < ~/crawl-data/directives_all.csv > ~/crawl-data/directives_all_clean.csv
-
-#bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
-#${filename}_sf_crawls.directives${now} ~/crawl-data/directives_all_clean.csv
-
-#if [ ! -f ~/crawl-data/sitemaps_all.csv ]; then
-    #echo "Error: sitemaps_all.csv does not exist."
-    #exit 1
-#fi
-#tr '\0' ' ' < ~/crawl-data/sitemaps_all.csv > ~/crawl-data/sitemaps_all_clean.csv
-#bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
-#${filename}_sf_crawls.sitemaps${now} ~/crawl-data/sitemaps_all_clean.csv
-
-#if [ ! -f ~/crawl-data/all_inlinks.csv ]; then
-    #echo "Error: all_inlinks.csv does not exist."
-    #exit 1
-#fi
-#tr '\0' ' ' < ~/crawl-data/all_inlinks.csv > ~/crawl-data/all_inlinks_clean.csv
-#bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
-#${filename}_sf_crawls.inlinks_${now} ~/crawl-data/all_inlinks_clean.csv
-
-#if [ ! -f ~/crawl-data/pagination_all.csv ]; then
-    #echo "Error: pagination_all.csv does not exist."
-    #exit 1
-#fi
-#tr '\0' ' ' < ~/crawl-data/pagination_all.csv > ~/crawl-data/pagination_all_clean.csv
-#bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
-#${filename}_sf_crawls.pagination${now} ~/crawl-data/pagination_all_clean.csv
-
-#if [ ! -f ~/crawl-data/hreflang_all.csv ]; then
-    #echo "Error: hreflang_all.csv does not exist."
-    #exit 1
-#fi
-#tr '\0' ' ' < ~/crawl-data/hreflang_all.csv > ~/crawl-data/hreflang_all_clean.csv 
-#bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
-#${filename}_sf_crawls.hreflang${now} ~/crawl-data/hreflang_all_clean.csv
-
-#if [ ! -f ~/crawl-data/structured_data_all.csv ]; then
-    #echo "Error: structured_data_all.csv does not exist."
-    #exit 1
-#fi
-#tr '\0' ' ' < ~/crawl-data/structured_data_all.csv > ~/crawl-data/structured_data_all_clean.csv
-#bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
-#${filename}_sf_crawls.structured_data${now} ~/crawl-data/structured_data_all_clean.csv
-
-
-if [ ! -f ~/crawl-data/content_all.csv ]; then
-    echo "Error: content_all.csv does not exist."
+if [ ! -f ~/crawl-data/internal_all.csv ]; then
+    echo "Error: internal_all.csv does not exist."
     exit 1
 fi
-tr '\0' ' ' < ~/crawl-data/content_all.csv > ~/crawl-data/content_all_clean.csv
+# replace null values with spaces in the csv files
+tr '\0' ' ' < ~/crawl-data/internal_all.csv > ~/crawl-data/internal_all_clean.csv
 bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
-${filename}_sf_crawls.content_all_${now} ~/crawl-data/content_all_clean.csv
+${filename}_sf_crawls.internal${now} ~/crawl-data/internal_all_clean.csv
+
+# get directives data
+if [ ! -f ~/crawl-data/directives_all.csv ]; then
+    echo "Error: directives_all.csv does not exist."
+    exit 1
+fi
+tr '\0' ' ' < ~/crawl-data/directives_all.csv > ~/crawl-data/directives_all_clean.csv
+
+bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
+${filename}_sf_crawls.directives${now} ~/crawl-data/directives_all_clean.csv
+
+# get sitemaps data
+if [ ! -f ~/crawl-data/sitemaps_all.csv ]; then
+    echo "Error: sitemaps_all.csv does not exist."
+    exit 1
+fi
+tr '\0' ' ' < ~/crawl-data/sitemaps_all.csv > ~/crawl-data/sitemaps_all_clean.csv
+bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
+${filename}_sf_crawls.sitemaps${now} ~/crawl-data/sitemaps_all_clean.csv
+
+#get all inlinks csv
+if [ ! -f ~/crawl-data/all_inlinks.csv ]; then
+    echo "Error: all_inlinks.csv does not exist."
+    exit 1
+fi
+tr '\0' ' ' < ~/crawl-data/all_inlinks.csv > ~/crawl-data/all_inlinks_clean.csv
+bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
+${filename}_sf_crawls.inlinks_${now} ~/crawl-data/all_inlinks_clean.csv
+
+#get pagination data
+if [ ! -f ~/crawl-data/pagination_all.csv ]; then
+    echo "Error: pagination_all.csv does not exist."
+    exit 1
+fi
+tr '\0' ' ' < ~/crawl-data/pagination_all.csv > ~/crawl-data/pagination_all_clean.csv
+bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
+${filename}_sf_crawls.pagination${now} ~/crawl-data/pagination_all_clean.csv
+
+# get hreflangs
+if [ ! -f ~/crawl-data/hreflang_all.csv ]; then
+    echo "Error: hreflang_all.csv does not exist."
+    exit 1
+fi
+tr '\0' ' ' < ~/crawl-data/hreflang_all.csv > ~/crawl-data/hreflang_all_clean.csv 
+bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
+${filename}_sf_crawls.hreflang${now} ~/crawl-data/hreflang_all_clean.csv
+
+# get structured data e.g. RDFAs 
+
+if [ ! -f ~/crawl-data/structured_data_all.csv ]; then
+    echo "Error: structured_data_all.csv does not exist."
+    exit 1
+fi
+tr '\0' ' ' < ~/crawl-data/structured_data_all.csv > ~/crawl-data/structured_data_all_clean.csv
+bq load --autodetect --source_format=CSV --allow_quoted_newlines --allow_jagged_rows --ignore_unknown_values \
+${filename}_sf_crawls.structured_data${now} ~/crawl-data/structured_data_all_clean.csv
 
 
 curl -i -H "Content-Type:application/json; charset=UTF-8" --data '{"text":"'"$domain"' crawl complete"}' "https://chat.googleapis.com/{token}"
